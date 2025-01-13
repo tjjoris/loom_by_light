50fd8d69fdb54abe777772fd43a96cb6c0d40c10
tjjoris <tjjoris@gmail.com>
2025-01-13 15:21:29 -0500

    created sketch header

--- loom_by_light/loom_by_light.ino
+++ loom_by_light/loom_by_light.ino
@@ -1,3 +1,17 @@
+/**
+*this sketch is intended to load a bitmap from an sd card and display each row of the bitmap's pixels in a 
+*addressable light strip. each pixel is converted into one of two values, because the purpose of this is
+for a custom loom, which allows the user to manually choose which of 2 colours to use on each loom heddle, 
+*thereby creating custom 2d loom images based off a bitmap.
+*The program is written by Luke Johnson, commissioned by Elizabeth Johnson, the organizer of the project.
+*some of the code was modified after being taken from bitsnbytes.co.uk:
+https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+*@author Luke Johnson
+*@date 2025-January-13
+*/
+
+
+
 void setup() {
   // put your setup code here, to run once:
 
