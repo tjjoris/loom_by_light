ea7af5dc8e81c6f2bb7c8f2c37f09fb2ac4e8814
tjjoris <tjjoris@gmail.com>
2025-01-14 17:59:11 -0500

    additional comments

--- loom_by_light/loom_by_light.ino
+++ loom_by_light/loom_by_light.ino
@@ -1,8 +1,8 @@
 /**
 *this sketch is intended to load a bitmap from an sd card and display each row of the bitmap's pixels in a 
-*addressable light strip. each pixel is converted into one of two values, because the purpose of this is
-for a custom loom, which allows the user to manually choose which of 2 colours to use on each loom heddle, 
-*thereby creating custom 2d loom images based off a bitmap.
+*addressable light strip as one of two values based on the saturation of the pixel.
+*The purpose of this is for a custom loom, which allows the user to manually choose which of 2 colours to use
+*on each loom heddle, thereby creating custom 2d loom images based off a bitmap.
 *The program is written by Luke Johnson, commissioned by Elizabeth Johnson, the organizer of the project.
 *some of the code was modified after being sourced from bitsnbytes.co.uk:
 https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
@@ -19,246 +19,246 @@ https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
 class BitmapHandler {
   //instance variables:
   private:
-    bool fileOK = false; //if file is ok to use
-    File bmpFile; //the file itself
-    String bmpFilename; //the file name
+  bool fileOK = false; //if file is ok to use
+  File bmpFile; //the file itself
+  String bmpFilename; //the file name
 
   public:
 
-    const int lightsArraySize = NUM_LIGHTS / 8;
-    //the byte array to store lights binary values.
-    byte lightsArray[NUM_LIGHTS / 8];
-    // BMP header fields
-    uint16_t headerField;
-    uint32_t fileSize;
-    uint32_t imageOffset;
-    // DIB header
-    uint32_t headerSize;
-    uint32_t imageWidth;
-    uint32_t imageHeight;
-    uint16_t colourPlanes;
-    uint16_t bitsPerPixel;
-    uint32_t compression;
-    uint32_t imageSize;
-    uint32_t xPixelsPerMeter;
-    uint32_t yPixelsPerMeter;
-    uint32_t totalColors;
-    uint32_t importantColors;
-    /**
-    *read a byte and return it as an unsigned 8 bit int.
-    * code was sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    uint8_t read8Bit(){
-      if (!this->bmpFile) {
-       return 0;
-      }
-      else {
-        return this->bmpFile.read();
-      }
-    }
+  const int lightsArraySize = NUM_LIGHTS / 8;
+  //the byte array to store lights binary values.
+  byte lightsArray[NUM_LIGHTS / 8];
+  // BMP header fields
+  uint16_t headerField;
+  uint32_t fileSize;
+  uint32_t imageOffset;
+  // DIB header
+  uint32_t headerSize;
+  uint32_t imageWidth;
+  uint32_t imageHeight;
+  uint16_t colourPlanes;
+  uint16_t bitsPerPixel;
+  uint32_t compression;
+  uint32_t imageSize;
+  uint32_t xPixelsPerMeter;
+  uint32_t yPixelsPerMeter;
+  uint32_t totalColors;
+  uint32_t importantColors;
 
-    /**
-    *read 2 bytes, and return them as an unsigned 16 bit int.
-    * code sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    uint16_t read16Bit(){
-      uint16_t lsb, msb;
-      if (!this->bmpFile) {
-       return 0;
-      }
-      else {
-        lsb = this->bmpFile.read();
-        msb = this->bmpFile.read();
-        return (msb<<8) + lsb;
-      }
+  /**
+  *read a byte and return it as an unsigned 8 bit int.
+  * code was sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  uint8_t read8Bit(){
+    if (!this->bmpFile) {
+      return 0;
     }
-
-    /**
-    *read 4 bytes, an return them as an unsigned 32 bit int.
-    * code was sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    uint32_t read32Bit(){
-      uint32_t lsb, b2, b3, msb;
-      if (!this->bmpFile) {
-       return 0;
-      }
-      else {
-        lsb = this->bmpFile.read();
-        b2 = this->bmpFile.read();
-        b3 = this->bmpFile.read();
-        msb = this->bmpFile.read();
-        return (msb<<24) + (b3<<16) + (b2<<8) + lsb;
-      }
+    else {
+      return this->bmpFile.read();
     }
+  }
 
-    /**
-    *constructor, sets instance variables for filename.
-    */
-    BitmapHandler(String filename){
+  /**
+  *read 2 bytes, and return them as an unsigned 16 bit int.
+  * code sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  uint16_t read16Bit(){
+    uint16_t lsb, msb;
+    if (!this->bmpFile) {
+      return 0;
+    }
+    else {
+      lsb = this->bmpFile.read();
+      msb = this->bmpFile.read();
+      return (msb<<8) + lsb;
+    }
+  }
+
+  /**
+  *read 4 bytes, an return them as an unsigned 32 bit int.
+  * code was sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  uint32_t read32Bit(){
+    uint32_t lsb, b2, b3, msb;
+    if (!this->bmpFile) {
+      return 0;
+    }
+    else {
+      lsb = this->bmpFile.read();
+      b2 = this->bmpFile.read();
+      b3 = this->bmpFile.read();
+      msb = this->bmpFile.read();
+      return (msb<<24) + (b3<<16) + (b2<<8) + lsb;
+    }
+  }
+
+  /**
+  *constructor, sets instance variables for filename.
+  */
+  BitmapHandler(String filename){
+    this->fileOK = false;
+    this->bmpFilename = filename;
+  }
+
+  /**
+  *verify the opened file.
+  * code modified after being sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  bool verifyFile() {
+    if (!this->bmpFile) {
+      Serial.print(F("BitmapHandler : Unable to open file "));
+      Serial.println(this->bmpFilename);
       this->fileOK = false;
-      this->bmpFilename = filename;
     }
-
-    /**
-    *verify the opened file.
-    * code modified after being sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    bool verifyFile() {
-      if (!this->bmpFile) {
-        Serial.print(F("BitmapHandler : Unable to open file "));
-        Serial.println(this->bmpFilename);
+    else {
+      if (!this->readFileHeaders()){
+        Serial.println(F("Unable to read file headers"));
         this->fileOK = false;
       }
       else {
-        if (!this->readFileHeaders()){
-          Serial.println(F("Unable to read file headers"));
+        if (!this->checkFileHeaders()){
+          Serial.println(F("Not compatible file"));
           this->fileOK = false;
         }
         else {
-          if (!this->checkFileHeaders()){
-            Serial.println(F("Not compatible file"));
-            this->fileOK = false;
-          }
-          else {
-            // all OK
-            Serial.println(F("BMP file all OK"));
-            this->fileOK = true;
-          }       
-        }
+          // all OK
+          Serial.println(F("BMP file all OK"));
+          this->fileOK = true;
+        }       
       }
     }
+  }
 
-    /**
-    *read file headers from file and set instance variables.
-    * code modified after being sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    bool readFileHeaders(){
-      if (this->bmpFile) {
-        // reset to start of file
-        this->bmpFile.seek(0);
-        
-        // BMP Header
-        this->headerField = this->read16Bit();
-        this->fileSize = this->read32Bit();
-        this->read16Bit(); // reserved
-        this->read16Bit(); // reserved
-        this->imageOffset = this->read32Bit();
-
-        // DIB Header
-        this->headerSize = this->read32Bit();
-        this->imageWidth = this->read32Bit();
-        this->imageHeight = this->read32Bit();
-        this->colourPlanes = this->read16Bit();
-        this->bitsPerPixel = this->read16Bit();
-        this->compression = this->read32Bit();
-        this->imageSize = this->read32Bit();
-        this->xPixelsPerMeter = this->read32Bit();
-        this->yPixelsPerMeter = this->read32Bit();
-        this->totalColors = this->read32Bit();
-        this->importantColors = this->read32Bit();
-        return true;
-      }
-      else {
-        return false;
-      }
-    }
-
-    /**
-    *check file header values are valid.
-    * code was sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    bool checkFileHeaders(){
-
-      // BMP file id
-      if (this->headerField != 0x4D42){
-        Serial.println("file is not Windows 3.1x, 95, NT, ... etc. bitmap file id.");
-        return false;
-      }
-      // must be single colour plane
-      if (this->colourPlanes != 1){
-        Serial.println("file is not single colour plane");
-        return false;
-      }
-      // only working with 24 bit bitmaps
-      if (this->bitsPerPixel != 24){
-        Serial.println("is not 24 bit bitmap.");
-        return false;
-      }
-      // no compression
-      if (this->compression != 0){
-        Serial.println("bitmap is compressed.");
-        return false;
-      }
-      // all ok
-      return true;
-    }
-
-    /**
-    *print file hader values to serial.
-    * code sourced from: 
-    *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
-    */
-    void serialPrintHeaders() {
-      Serial.print(F("filename : "));
-      Serial.println(this->bmpFilename);
+  /**
+  *read file headers from file and set instance variables.
+  * code modified after being sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  bool readFileHeaders(){
+    if (this->bmpFile) {
+      // reset to start of file
+      this->bmpFile.seek(0);
+      
       // BMP Header
-      Serial.print(F("headerField : "));
-      Serial.println(this->headerField, HEX);
-      Serial.print(F("fileSize : "));
-      Serial.println(this->fileSize);
-      Serial.print(F("imageOffset : "));
-      Serial.println(this->imageOffset);
-      Serial.print(F("headerSize : "));
-      Serial.println(this->headerSize);
-      Serial.print(F("imageWidth : "));
-      Serial.println(this->imageWidth);
-      Serial.print(F("imageHeight : "));
-      Serial.println(this->imageHeight);
-      Serial.print(F("colourPlanes : "));
-      Serial.println(this->colourPlanes);
-      Serial.print(F("bitsPerPixel : "));
-      Serial.println(this->bitsPerPixel);
-      Serial.print(F("compression : "));
-      Serial.println(this->compression);
-      Serial.print(F("imageSize : "));
-      Serial.println(this->imageSize);
-      Serial.print(F("xPixelsPerMeter : "));
-      Serial.println(this->xPixelsPerMeter);
-      Serial.print(F("yPixelsPerMeter : "));
-      Serial.println(this->yPixelsPerMeter);
-      Serial.print(F("totalColors : "));
-      Serial.println(this->totalColors);
-      Serial.print(F("importantColors : "));
-      Serial.println(this->importantColors);
-    }
+      this->headerField = this->read16Bit();
+      this->fileSize = this->read32Bit();
+      this->read16Bit(); // reserved
+      this->read16Bit(); // reserved
+      this->imageOffset = this->read32Bit();
 
-    /**
-    *open the file, print an error message if it is not opened. return true if the file is opened, otherwise false.
-    */
-    bool openFile() {
-    this->bmpFile = SD.open(this->bmpFilename, FILE_READ);
-    if (!this->bmpFile) {
-        Serial.print(F("BitmapHandler : Unable to open file "));
-        Serial.println(this->bmpFilename);
-        this->fileOK = false;
-        return false;
-      }
+      // DIB Header
+      this->headerSize = this->read32Bit();
+      this->imageWidth = this->read32Bit();
+      this->imageHeight = this->read32Bit();
+      this->colourPlanes = this->read16Bit();
+      this->bitsPerPixel = this->read16Bit();
+      this->compression = this->read32Bit();
+      this->imageSize = this->read32Bit();
+      this->xPixelsPerMeter = this->read32Bit();
+      this->yPixelsPerMeter = this->read32Bit();
+      this->totalColors = this->read32Bit();
+      this->importantColors = this->read32Bit();
       return true;
     }
-
-    /**
-    *close the file.
-    */
-    void closeFile() {
-      this->bmpFile.close();
+    else {
+      return false;
     }
+  }
 
+  /**
+  *check file header values are valid.
+  * code was sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  bool checkFileHeaders(){
+
+    // BMP file id
+    if (this->headerField != 0x4D42){
+      Serial.println("file is not Windows 3.1x, 95, NT, ... etc. bitmap file id.");
+      return false;
+    }
+    // must be single colour plane
+    if (this->colourPlanes != 1){
+      Serial.println("file is not single colour plane");
+      return false;
+    }
+    // only working with 24 bit bitmaps
+    if (this->bitsPerPixel != 24){
+      Serial.println("is not 24 bit bitmap.");
+      return false;
+    }
+    // no compression
+    if (this->compression != 0){
+      Serial.println("bitmap is compressed.");
+      return false;
+    }
+    // all ok
+    return true;
+  }
+
+  /**
+  *print file hader values to serial.
+  * code sourced from: 
+  *https://bytesnbits.co.uk/bitmap-image-handling-arduino/#google_vignette
+  */
+  void serialPrintHeaders() {
+    Serial.print(F("filename : "));
+    Serial.println(this->bmpFilename);
+    // BMP Header
+    Serial.print(F("headerField : "));
+    Serial.println(this->headerField, HEX);
+    Serial.print(F("fileSize : "));
+    Serial.println(this->fileSize);
+    Serial.print(F("imageOffset : "));
+    Serial.println(this->imageOffset);
+    Serial.print(F("headerSize : "));
+    Serial.println(this->headerSize);
+    Serial.print(F("imageWidth : "));
+    Serial.println(this->imageWidth);
+    Serial.print(F("imageHeight : "));
+    Serial.println(this->imageHeight);
+    Serial.print(F("colourPlanes : "));
+    Serial.println(this->colourPlanes);
+    Serial.print(F("bitsPerPixel : "));
+    Serial.println(this->bitsPerPixel);
+    Serial.print(F("compression : "));
+    Serial.println(this->compression);
+    Serial.print(F("imageSize : "));
+    Serial.println(this->imageSize);
+    Serial.print(F("xPixelsPerMeter : "));
+    Serial.println(this->xPixelsPerMeter);
+    Serial.print(F("yPixelsPerMeter : "));
+    Serial.println(this->yPixelsPerMeter);
+    Serial.print(F("totalColors : "));
+    Serial.println(this->totalColors);
+    Serial.print(F("importantColors : "));
+    Serial.println(this->importantColors);
+  }
+
+  /**
+  *open the file, print an error message if it is not opened. return true if the file is opened, otherwise false.
+  */
+  bool openFile() {
+  this->bmpFile = SD.open(this->bmpFilename, FILE_READ);
+  if (!this->bmpFile) {
+      Serial.print(F("BitmapHandler : Unable to open file "));
+      Serial.println(this->bmpFilename);
+      this->fileOK = false;
+      return false;
+    }
+    return true;
+  }
+
+  /**
+  *close the file.
+  */
+  void closeFile() {
+    this->bmpFile.close();
+  }
 
   /**
   *is passed a row number, sets array of bytes, representing if each pixel is true or
@@ -387,6 +387,10 @@ void printBinary(byte b) {
   }
 }
 
+/**
+*creates bitmap handler object, then opens bitmap file, reads the headers, then loops each row in the bitmap
+*and decodes each row, printing the binary values based on saturation.
+*/
 void setup() {
   //set serial to dispaly on ide
   Serial.begin(9600);
